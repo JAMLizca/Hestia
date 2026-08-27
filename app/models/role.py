fcb41d22a3a3f0af58d05c0e1dca0f4526b2604e
JAMLizca <josemontenegroqc@gmail.com>
2026-08-27 11:12:44 -0500

    chore: models de la tabla roles

--- app/models/role.py
+++ app/models/role.py
@@ -1,9 +1,11 @@
 """
 Modelo SQLAlchemy de la tabla roles.
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, column
 from app.db.base_class import Base
 
 class Role(Base):
      __tablename__="roles"
-     
+     id=column(Integer, primary_key=True, index=True)
+     nombre=column(String(50), unique=True, nullable=False)
+     description=column(String(255), nullable=True)
