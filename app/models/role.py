7801dfbf0ab3557d7f40fee010522dd0caeccae5
JAMLizca <josemontenegroqc@gmail.com>
2026-08-27 20:39:22 -0500

    fix:modelo Role, Column() de SQLAlchemy

--- app/models/role.py
+++ app/models/role.py
@@ -1,11 +1,11 @@
 """
 Modelo SQLAlchemy de la tabla roles.
 """
-from sqlalchemy import Column, Integer, String, column
+from sqlalchemy import Column, Integer, String, Column
 from app.db.base_class import Base
 
 class Role(Base):
      __tablename__="roles"
-     id=column(Integer, primary_key=True, index=True)
-     nombre=column(String(50), unique=True, nullable=False)
-     description=column(String(255), nullable=True)
+     id=Column(Integer, primary_key=True, index=True)
+     nombre=Column(String(50), unique=True, nullable=False)
+     description=Column(String(255), nullable=True)
