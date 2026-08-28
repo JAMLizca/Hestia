150eac5dbe831da92bd89134414ab5efe1f5cbac
JAMLizca <josemontenegroqc@gmail.com>
2026-08-27 20:57:22 -0500

    fix:helth.py line 10

--- app/api/routes/health.py
+++ app/api/routes/health.py
@@ -7,9 +7,11 @@ from app.api.deps import get_db
 
 router = APIRouter(tags=["Salud"])
 
-@router.get("/health"
-            summary="Estado de la API y conexión a la DB",
-            response_description="Estado de la API y conexión a la DB")
+@router.get(
+    "/health",
+    summary="Verifica el estado de la API y la conexión a la base de datos",
+    response_description="Estado de la API y de MySQL",
+)
 
 def health_check(db: Session = Depends(get_db)):
 
